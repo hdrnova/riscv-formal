9e370841c8c1f5c0e948e776daed6e351e3185e1
dopamane <dwc1295@gmail.com>
2019-06-16 21:22:42 -0400

    troubleshoot insn check config and wrapper

--- cores/veldt/wrapper.sv
+++ cores/veldt/wrapper.sv
@@ -5,13 +5,11 @@ module rvfi_wrapper (
 );
 	(* keep *) `rvformal_rand_reg [31:0] mem_rdata;
 	(* keep *) wire               [31:0] mem_instr;
-   
-        `RVFI_WIRES
-	  
+
 	VeldtFV uut (
 		.clock     (clock),
 		.reset     (reset),
-                .word      (mem_instr),
+    .word      (mem_instr),
 		.ramOut    (mem_rdata),
 		`RVFI_CONN
 	);
