22c11e408998d671ec486fca6b9bc73c0067bcbe
dopamane <dwc1295@gmail.com>
2019-06-20 21:41:00 -0400

    update wrapper

--- cores/veldt/wrapper.sv
+++ cores/veldt/wrapper.sv
@@ -5,12 +5,14 @@ module rvfi_wrapper (
 );
 	(* keep *) wire [31:0] instr;
 	(* keep *) wire [31:0] ramOut;
+	(* keep *) wire [31:0] ramIn;
 
 	VeldtFV uut (
 	  .clock     (clock),
 	  .reset     (reset),
     .word      (instr),
 		.ramOut    (ramOut),
+		.ramIn     (ramIn),
 	  `RVFI_CONN
 	);
 
