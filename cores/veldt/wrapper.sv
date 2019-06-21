ce672555f5702232261d91ca8e4c7b850fe4290d
dopamane <dwc1295@gmail.com>
2019-06-20 23:48:10 -0400

    remove ram

--- cores/veldt/wrapper.sv
+++ cores/veldt/wrapper.sv
@@ -4,16 +4,12 @@ module rvfi_wrapper (
 	`RVFI_OUTPUTS
 );
 	(* keep *) wire [31:0] instr;
-	(* keep *) wire [31:0] ramOut;
-	(* keep *) wire [31:0] ramIn;
-
+	
 	VeldtFV uut (
 	  .clock     (clock),
 	  .reset     (reset),
     .word      (instr),
-		.ramOut    (ramOut),
-		.ramIn     (ramIn),
-	  `RVFI_CONN
+		`RVFI_CONN
 	);
 
 endmodule
