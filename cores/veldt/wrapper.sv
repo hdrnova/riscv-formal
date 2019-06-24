3899daa75dbe2f341452127262917a473f2a8b36
dopamane <dwc1295@gmail.com>
2019-06-24 03:08:34 -0400

    expose pc in wrapper

--- cores/veldt/wrapper.sv
+++ cores/veldt/wrapper.sv
@@ -5,12 +5,14 @@ module rvfi_wrapper (
 );
 	(* keep *) `rvformal_rand_reg [31:0] instr;
 	(* keep *) `rvformal_rand_reg [31:0] ramOut;
+	(* keep *) wire               [31:0] pc;
 
 	VeldtFV uut (
 	  .clock     (clock),
 	  .reset     (reset),
     .word      (instr),
 		.ramOut    (ramOut),
+		.pc        (pc),
 		`RVFI_CONN
 	);
 
