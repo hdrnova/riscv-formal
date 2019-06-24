d76bcd789d4cfdadeea4acf29c1fa19f06675773
dopamane <dwc1295@gmail.com>
2019-06-24 07:02:14 -0400

    update wrapper, expose ram inputs

--- cores/veldt/wrapper.sv
+++ cores/veldt/wrapper.sv
@@ -6,12 +6,14 @@ module rvfi_wrapper (
 	(* keep *) `rvformal_rand_reg [31:0] instr;
 	(* keep *) `rvformal_rand_reg [31:0] ramOut;
 	(* keep *) wire               [31:0] pc;
+	(* keep *) wire               [96:0] ramIn;
 
 	VeldtFV uut (
 	  .clock     (clock),
 	  .reset     (reset),
     .word      (instr),
 		.ramOut    (ramOut),
+		.ramIn     (ramIn),
 		.pc        (pc),
 		`RVFI_CONN
 	);
