69529373d037efa7d6e82327545fd9559f968537
dopamane <dwc1295@gmail.com>
2019-10-13 19:05:44 -0400

    update wrapper interface

--- cores/veldt/wrapper.sv
+++ cores/veldt/wrapper.sv
@@ -3,14 +3,22 @@ module rvfi_wrapper (
 	input         reset,
 	`RVFI_OUTPUTS
 );
-	(* keep *) `rvformal_rand_reg [31:0] ramOut;
-	(* keep *) wire               [96:0] ramIn;
+	(* keep *) `rvformal_rand_reg [31:0] mem_rdata;
+   (* keep *) wire               mem_valid;
+   (* keep *) wire mem_instr;
+   (* keep *) wire [31:0] mem_addr;
+   (* keep *) wire [31:0] mem_wdata;
+   (* keep *) wire [3:0] mem_wstrb;
 
 	VeldtFV uut (
 	  .clock     (clock),
 	  .reset     (reset),
-		.ramOut    (ramOut),
-		.ramIn     (ramIn),
+		     .mem_valid (mem_valid),
+		     .mem_instr (mem_instr),
+		     .mem_addr  (mem_addr),
+		     .mem_wdata (mem_wdata),
+		     .mem_wstrb (mem_wstrb),
+		     .mem_rdata (mem_rdata),
 		`RVFI_CONN
 	);
 
