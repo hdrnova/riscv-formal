b97ec4b69cecd0d4f9a894074cde0cdda267f486
dopamane <dwc1295@gmail.com>
2021-02-23 15:42:14 -0500

    mem_ready wire

--- cores/lion/wrapper.sv
+++ cores/lion/wrapper.sv
@@ -3,18 +3,22 @@ module rvfi_wrapper (
 	input         reset,
 	`RVFI_OUTPUTS
 );
-	(* keep *) `rvformal_rand_reg [31:0] mem_rdata;
-   (* keep *) wire               mem_valid;
-   (* keep *) wire mem_instr;
+   
+   (* keep *) `rvformal_rand_reg        mem_ready;
+   (* keep *) `rvformal_rand_reg [31:0] mem_rdata;
+
+   (* keep *) wire        mem_valid;
+   (* keep *) wire        mem_instr;
    (* keep *) wire [31:0] mem_addr;
    (* keep *) wire [31:0] mem_wdata;
-   (* keep *) wire [3:0] mem_wstrb;
+   (* keep *) wire [3:0]  mem_wstrb;
 
 	LionFV uut (
 	  .clock     (clock),
 	  .reset     (reset),
 		     .mem_valid (mem_valid),
 		     .mem_instr (mem_instr),
+                     .mem_ready (mem_ready),
 		     .mem_addr  (mem_addr),
 		     .mem_wdata (mem_wdata),
 		     .mem_wstrb (mem_wstrb),
